348f0ce7798c8ee2ab51b09891859b73d97bd154
quabid <quabidlord@gmail.com>
2020-10-10 12:07:31 -0400

    Modified server send headers

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,38 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import express from 'express';
 import chalk from 'chalk';
+import { customAlphabet } from 'nanoid';
 import ProductRoutes from './routes/ProductRoutes.js';
 
 dotenv.config();
 connectDB();
 
+const nanoid = customAlphabet('02468ouqtyminv', 13);
 const PORT = process.env.PORT || 5000;
 const app = express();
+app.use((req, res, next) => {
+  res.type('json');
+
+  res.set({
+    'Content-Type': 'application/json',
+    'Content-Length': '123',
+    'x-powered-by': 'Deez Nuts!@$?%&#',
+    'Cache-control': 'no-cache,no-store,max-age=0,must-revalidate',
+    'X-XSS-Protection': '1;mode=block',
+    'X-Content-Type-Options': 'nosniff',
+    ETag: nanoid(),
+  });
+
+  res.setHeader('Cache-Control', 'no-cache,no-store,max-age=0,must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '-1');
+  res.setHeader('X-XSS-Protection', '1;mode=block');
+  // res.setHeader('Content-Type', 'text/json; charset=utf-8');
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  res.setHeader('Content-Security-Policy', "script-src 'self'");
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.status(200).send(`API is running`);
